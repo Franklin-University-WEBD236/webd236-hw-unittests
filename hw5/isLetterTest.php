a2b2914108bf9d424a6e150f295161af8c6cd9aa
Tyler Whitney <tylerjwhitney@gmail.com>
2021-01-25 11:42:15 -0500

    Create isLetterTest.php

---
+++ hw5/isLetterTest.php
@@ -0,0 +1,23 @@
+<?php
+
+include "code.php";
+
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Assert;
+
+final class CodeTest extends TestCase {
+
+  public function testIsLetterWithNumber() {
+    $this->assertFalse(isLetter("1"), "isLetter('1') should be false");
+  }
+  public function testIsLetterLowerCaseEdge() {
+    $this->assertTrue(isLetter("z"), "isLetter('z') should be true");
+  }
+  public function testIsLetterCapitalEdge() {
+    $this->assertTrue(isLetter("A"), "isLetter('A') should be true");
+  }
+  public function testIsLetterTest() {
+    $this->assertTrue(isLetter("g"), "isLetter('g') should be true");
+  }
+
+?>
