de11e6aaa2359d65db036cb15ca52dc0f6a0da40
Tyler Whitney <tylerjwhitney@gmail.com>
2021-02-25 23:09:27 -0500

    Update modeMakerTest.php

--- hw7/modeMakerTest.php
+++ hw7/modeMakerTest.php
@@ -1,7 +1,19 @@
-function test_modeMaker() {
-  $arr = [2, 8, 10, 5, 3, 5, 1, 2, 5, 7, 4];
-  assertEquals(5, reduce($arr, modeMaker()), "mode should be 5");
-  $arr[] = 2;
-  $arr[] = 2;
-  assertEquals(2, reduce($arr, modeMaker()), "mode should be 2");
+
+<?php
+
+include "code.php";
+
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Assert;
+
+final class reduceTest extends TestCase {
+  function test_modeMaker() {
+    $arr = [2, 8, 10, 5, 3, 5, 1, 2, 5, 7, 4];
+    assertEquals(5, reduce($arr, modeMaker()), "mode should be 5");
+    $arr[] = 2;
+    $arr[] = 2;
+    assertEquals(2, reduce($arr, modeMaker()), "mode should be 2");
+  }
 }
+
+?>
