2ef292fd58a5e9c9cb93b0172a284806b0511e46
Tyler Whitney <tylerjwhitney@gmail.com>
2021-02-25 23:14:57 -0500

    Update 1modeMakerTest.php

--- hw7/1modeMakerTest.php
+++ hw7/1modeMakerTest.php
@@ -10,10 +10,10 @@ final class modeMakerTest extends TestCase {
   
   function test_modeMaker() {
     $arr = [2, 8, 10, 5, 3, 5, 1, 2, 5, 7, 4];
-    $this->assertEquals(5, reduce($arr, modeMaker()), "mode should be 5");
+    $this->assertEquals(5, array_reduce($arr, modeMaker()), "mode should be 5");
     $arr[] = 2;
     $arr[] = 2;
-    $this->assertEquals(2, reduce($arr, modeMaker()), "mode should be 2");
+    $this->assertEquals(2, array_reduce($arr, modeMaker()), "mode should be 2");
   }
   
 }
