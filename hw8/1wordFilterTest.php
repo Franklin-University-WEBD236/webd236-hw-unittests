0e4437ae1620c879538882c3ec9dd75262813668
Tyler Whitney <tylerjwhitney@gmail.com>
2021-03-02 15:42:45 -0500

    Update and rename wordFilterTest.php to 1wordFilterTest.php

--- hw8/1wordFilterTest.php
+++ hw8/1wordFilterTest.php
@@ -9,7 +9,7 @@ final class filterWordTest extends TestCase {
   
   function test_wordFilter1() {
     $this->assertEquals("*****", wordFilter("fudge"));
-    $this->assertEquals("Oh, *****!, I'm in trouble now!", wordFilter("Oh, shoot! I'm in trouble now!"));
+    $this->assertEquals("Oh, *****! I'm in trouble now!", wordFilter("Oh, shoot! I'm in trouble now!"));
     $this->assertEquals("**** it! I goofed again.", wordFilter("Darn it! I goofed again."));
     $this->assertEquals("Get the ***** outta here! I'm tired of all your gosh **** desserts.", wordFilter("Get the fudge outta here! I'm tired of all your gosh darn desserts."));
   }
