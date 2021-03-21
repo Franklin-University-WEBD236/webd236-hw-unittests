7f8674671056f44ed47fcc5c002212c400f6d69a
Tyler Whitney <tylerjwhitney@gmail.com>
2021-03-21 03:20:44 -0400

    Update fizzBuzzServiceTest.php

--- hw9/fizzBuzzServiceTest.php
+++ hw9/fizzBuzzServiceTest.php
@@ -6,14 +6,20 @@ use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\Assert;
 
 final class fizzBuzzServiceTest extends TestCase {
-  
   function test_getFizzBuzz() {
-    $results1 = file_get_contents("results1.txt")
-    foreach ($data as $ipa) {
-      $this->assertTrue(matchIPAddress($ipa), "$ipa should be a valid IP address");
+    $base = "https://raw.githubusercontent.com/twhitney11/webd236-hw-unittests/main/hw9/";
+    $c = 1;
+    $results = [];
+    while($c < 6){
+     $results[$c] = file_get_contents($base . "results" . $c . ".txt";
+     $c++; 
     }
+    $this->assertEquals($results[1], getFizzBuzz(1,15), "The HTML output is not correct, expeccted: " . $results[1]);
+    $this->assertEquals($results[2], getFizzBuzz(1,30), "The HTML output is not correct, expeccted: " . $results[2]);
+    $this->assertEquals($results[3], getFizzBuzz(9,30), "The HTML output is not correct, expeccted: " . $results[3]);
+    $this->assertEquals($results[4], getFizzBuzz(5,10), "The HTML output is not correct, expeccted: " . $results[4]);
+    $this->assertEquals($results[5], getFizzBuzz(3,9), "The HTML output is not correct, expeccted: " . $results[5]);
   }
-  
 }
 
 ?>
