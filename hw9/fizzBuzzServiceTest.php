abd4ef49820fe72c48a3e973983e27b36a6e316b
Tyler Whitney <tylerjwhitney@gmail.com>
2021-03-21 03:05:33 -0400

    Update and rename testFizzBuzzService.php to fizzBuzzServiceTest.php

---
+++ hw9/fizzBuzzServiceTest.php
@@ -0,0 +1,20 @@
+<?php
+
+include "code.php";
+
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Assert;
+
+final class fizzBuzzServiceTest extends TestCase {
+  
+  function test_getFizzBuzz() {
+    
+    $html = file_get_contents("test1.txt")
+    foreach ($data as $ipa) {
+      $this->assertTrue(matchIPAddress($ipa), "$ipa should be a valid IP address");
+    }
+  }
+  
+}
+
+?>
